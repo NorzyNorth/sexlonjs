28187e0302b8043d9b5fc2a65054ed8d9491318a
Rais3322 <Rais3322@yandex.ru>
2024-01-22 16:33:41 +0300

    UTKA PADAET

--- src/client/engine/classes/entities/Player.ts
+++ src/client/engine/classes/entities/Player.ts
@@ -12,26 +12,20 @@ export default class Player extends Actor {
 	private movementX: number = 0;
 	private movementY: number = 0;
 	private movementZ: number = 0;
-	private movementSpeed: number = 0.2;
+	private movementSpeed: number = 8;
 	private gravity: number = 9.8;
-	private jumpPower: number = 1;
+	private jumpPower: number = 4;
 	private gameInput: GameInput;
 	private ray: Ray;
 	private rayHelper: RayHelper;
 	private isOnGround: boolean = false;
-	// private capsuleAgregate: PhysicsAggregate
 	private collider: PhysicsBody;
 	constructor(name: string, scene: Scene) {
 		super(name, scene)
-		// this.capsuleAgregate = new PhysicsAggregate(this.root, PhysicsShapeType.CAPSULE, { mass: 10, restitution: 0.1, radius: 0.8, pointA: new Vector3(0, 1.25, 0), pointB: new Vector3(0, 0.7, 0) }, this.scene);
-		// this.capsuleAgregate.body.setMassProperties({
-		// 	inertia: new Vector3(0, 0, 0)
-		// });
-		// this.capsuleAgregate.body.disablePreStep = false
 		this.name = name;
 		this.scene = scene;
 		this.gameInput = new GameInput(scene);
-		this.ray = new Ray(new Vector3(this.root.position._x, this.root.position._y, this.root.position._z), new Vector3(0, -1, 0), 0.01);
+		this.ray = new Ray(new Vector3(this.root.position._x, this.root.position._y + 0.005, this.root.position._z), new Vector3(0, -1, 0), 0.1);
 		this.rayHelper = new RayHelper(this.ray);
 		this.rayHelper.show(this.scene, new Color3(255, 0, 0));
 
@@ -40,13 +34,10 @@ export default class Player extends Actor {
 
 	
 	onTick = (deltaTime: number) => {
-		// console.log()
 		if (deltaTime) {
 			this.updatePosition();
 			this.isOnGround = this.checkGroundCollision();
 			this.move(deltaTime);
-			console.log(this.isOnGround)
-			// console.log(this.movementY)
 			this.jump();
 		}
 	};
@@ -63,10 +54,8 @@ export default class Player extends Actor {
 	}
 
 	private checkGroundCollision = () => {
-		this.ray.origin = new Vector3(this.root.position._x, this.root.position._y, this.root.position._z)
+		this.ray.origin = new Vector3(this.root.position._x, this.root.position._y + 0.005, this.root.position._z)
 		const hitInfo: PickingInfo = this.scene.pickWithRay(this.ray, (mesh: AbstractMesh) => !(mesh == this.root))
-		// console.log(`${this.ray.origin._x} , ${this.rayHelper.ray.origin._x}, ${this.root.position._x}, ${this.position._x}, ${this.meshAssetsMap.get(this.name)?.loadedMeshes[0].position}`)
-		// console.log(this.scene.getNodeById('Pivot'))
 		return hitInfo.hit ? true : false
 	}
 
@@ -83,8 +72,9 @@ export default class Player extends Actor {
 			// if (!this.isOnGround) {
 			// 	this.movementY -= this.gravity * this.scene.deltaTime / 10000;
 			// }
-			console.log(this.collider.getGravityFactor());
-			const movement = new Vector3(this.movementX, -9.8, this.movementZ);
+			this.movementY += this.scene.gravity.y * this.scene.deltaTime;
+			console.log(this.movementY);
+			const movement = new Vector3(this.movementX, this.movementY, this.movementZ);
 			// this.root.moveWithCollisions(movement);
 			this.collider.setLinearVelocity(movement);
 		};
@@ -92,12 +82,11 @@ export default class Player extends Actor {
 
 	private jump = () => {
 		if (this.gameInput.checkjumpInput() && this.isOnGround) {
-			this.collider.applyImpulse(new Vector3(0, this.jumpPower, 0), this.root.position);
+			this.collider.applyImpulse(new Vector3(0, this.jumpPower, 0), this.root.position, 5);
 		}
 	}
 
 	private createCollider = () => {
-		// const capsuleAgregate = new PhysicsAggregate(this.root, PhysicsShapeType.CAPSULE, { mass: 1, restitution: 0, radius: 0.7, pointA: new Vector3(0, 0.7, 0), pointB: new Vector3(0, 1.4, 0) }, this.scene);
 		this.collider = new PhysicsBody(this.root, PhysicsMotionType.DYNAMIC, false, this.scene);
 		this.collider.shape = new PhysicsShapeCylinder(new Vector3(0, 0, 0), new Vector3(0, 2, 0), 0.7, this.scene);
 		this.collider.shape.material = {restitution: 0}
