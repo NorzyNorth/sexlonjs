f6d038aeba31c83f02bafcba4c0f04e94504e91b
Rais3322 <Rais3322@yandex.ru>
2024-01-22 14:32:57 +0300

    UTKA

--- src/client/engine/classes/entities/Player.ts
+++ src/client/engine/classes/entities/Player.ts
@@ -1,4 +1,4 @@
-import { AbstractMesh, Color3, MeshBuilder, PhysicsAggregate, PhysicsBody, PhysicsMotionType, PhysicsShapeCapsule, PhysicsShapeType, PickingInfo, Ray, Scene, Vector3 } from "@babylonjs/core";
+import { AbstractMesh, Color3, MeshBuilder, PhysicsAggregate, PhysicsBody, PhysicsMotionType, PhysicsShapeCapsule, PhysicsShapeCylinder, PhysicsShapeType, PickingInfo, Ray, RayHelper, Scene, Vector3 } from "@babylonjs/core";
 import { Actor } from "../GameFramework";
 import GameInput from "client/scripts/GameInput";
 
@@ -10,24 +10,27 @@ export default class Player extends Actor {
 	private movementX: number = 0;
 	private movementY: number = 0;
 	private movementZ: number = 0;
-	private movementSpeed: number = 0.2;
-	private gravity: number = 9.8;
+	private movementSpeed: number = 7;
 	private jumpPower: number = 1;
 	private gameInput: GameInput;
 	private ray: Ray;
+	private rayHelper: RayHelper;
 	private isOnGround: boolean = false;
-	private capsuleAgregate: PhysicsAggregate
+	// private capsuleAgregate: PhysicsAggregate
+	private collider: PhysicsBody;
 	constructor(name: string, scene: Scene) {
 		super(name, scene)
-		this.capsuleAgregate = new PhysicsAggregate(this.root, PhysicsShapeType.CAPSULE, { mass: 10, restitution: 0.1, radius: 0.8, pointA: new Vector3(0, 1.25, 0), pointB: new Vector3(0, 0.7, 0) }, this.scene);
-		this.capsuleAgregate.body.setMassProperties({
-			inertia: new Vector3(0, 0, 0)
-		});
-		this.capsuleAgregate.body.disablePreStep = false
+		// this.capsuleAgregate = new PhysicsAggregate(this.root, PhysicsShapeType.CAPSULE, { mass: 10, restitution: 0.1, radius: 0.8, pointA: new Vector3(0, 1.25, 0), pointB: new Vector3(0, 0.7, 0) }, this.scene);
+		// this.capsuleAgregate.body.setMassProperties({
+		// 	inertia: new Vector3(0, 0, 0)
+		// });
+		// this.capsuleAgregate.body.disablePreStep = false
 		this.name = name;
 		this.scene = scene;
 		this.gameInput = new GameInput(scene);
-		this.ray = new Ray(new Vector3(this.root.position._x, this.root.position._y, this.root.position._z), new Vector3(0, -1, 0), 0.2);
+		this.ray = new Ray(new Vector3(this.root.position._x, this.root.position._y, this.root.position._z), new Vector3(0, -1, 0), 0.01);
+		this.rayHelper = new RayHelper(this.ray);
+		this.rayHelper.show(this.scene, new Color3(255, 0, 0));
 
 		this.addMeshAssetTask(this.name, "models/", "Duck.glb");
 	}
@@ -46,7 +49,7 @@ export default class Player extends Actor {
 	onBeginPlay = () => {
 		console.log('DuckActor onBeginPlay');
 		this.meshAssetsMap.get(this.name)?.loadedMeshes[0].position.set(0, 0, 0);
-		// this.createCollider();
+		this.createCollider();
 		this.updatePosition();
 	};
 
@@ -55,7 +58,7 @@ export default class Player extends Actor {
 	}
 
 	private checkGroundCollision = () => {
-		this.ray.origin = new Vector3(this.root.position._x, this.root.position._y - 0.1, this.root.position._z)
+		this.ray.origin = new Vector3(this.root.position._x, this.root.position._y, this.root.position._z)
 		const hitInfo: PickingInfo = this.scene.pickWithRay(this.ray, (mesh: AbstractMesh) => !(mesh == this.root))
 		return hitInfo.hit ? true : false
 	}
@@ -73,24 +76,24 @@ export default class Player extends Actor {
 			// if (!this.isOnGround) {
 			// 	this.movementY -= this.gravity * this.scene.deltaTime / 10000;
 			// }
-			const movement = new Vector3(this.movementX, this.movementY, this.movementZ);
-			this.root.moveWithCollisions(movement);
+			console.log(this.collider.getGravityFactor());
+			const movement = new Vector3(this.movementX, -9.8, this.movementZ);
+			// this.root.moveWithCollisions(movement);
+			this.collider.setLinearVelocity(movement);
 		};
 	};
 
 	private jump = () => {
 		if (this.gameInput.checkjumpInput() && this.isOnGround) {
-			this.movementY = this.jumpPower;
-		} else {
-			this.movementY = 0; 
+			this.collider.applyImpulse(new Vector3(0, this.jumpPower, 0), this.root.position);
 		}
 	}
 
 	private createCollider = () => {
 		// const capsuleAgregate = new PhysicsAggregate(this.root, PhysicsShapeType.CAPSULE, { mass: 1, restitution: 0, radius: 0.7, pointA: new Vector3(0, 0.7, 0), pointB: new Vector3(0, 1.4, 0) }, this.scene);
-		const body = new PhysicsBody(this.root, PhysicsMotionType.DYNAMIC, false, this.scene);
-		body.shape = new PhysicsShapeCapsule(new Vector3(0, 0.7, 0), new Vector3(0, 1.4, 0), 0.7, this.scene);
-		body.shape.material = {restitution: 0}
-		body.disablePreStep = false;
+		this.collider = new PhysicsBody(this.root, PhysicsMotionType.DYNAMIC, false, this.scene);
+		this.collider.shape = new PhysicsShapeCylinder(new Vector3(0, 0, 0), new Vector3(0, 2, 0), 0.7, this.scene);
+		this.collider.shape.material = {restitution: 0}
+		// this.collider.disablePreStep = true;
 	}
 };
